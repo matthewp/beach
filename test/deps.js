bf3f102632c3996ad8b8fa148be88f8155e5d85f
Matthew Phillips <matthew@phillipsoft.com>
2021-07-12 08:58:25 -0400

    Support lit elements with attributes

--- test/deps.js
+++ test/deps.js
@@ -1,1 +1,2 @@
 export * from 'https://deno.land/std@0.100.0/testing/asserts.ts';
+export { parseHTML } from 'https://unpkg.com/linkedom@0.11.0/worker.js';
--- test/libraries/lit.test.js
+++ test/libraries/lit.test.js
@@ -2,7 +2,8 @@ import '../../lib/shim.js?global';
 import { LitElement, html as litHtml } from 'https://cdn.skypack.dev/lit@2.0.0-rc.2';
 import { html, render } from '../../lib/mod.js';
 import { consume } from '../helpers.js';
-import { assertStringIncludes } from '../deps.js';
+import { assertStringIncludes, assertEquals, parseHTML } from '../deps.js';
+import { customElements } from '../../lib/dom.js';
 
 Deno.test('Can render a lit app', async () => {
   class MyElement extends LitElement {
@@ -12,14 +13,31 @@ Deno.test('Can render a lit app', async () => {
       `;
     }
   }
-  customElements.define('my-element-test', MyElement);
+  customElements.define('my-lit-test', MyElement);
 
-  const iter = html`
+  let iter = html`
     <div id="outer">
-      ${render`<my-element-test></my-element-test>`}
+      ${render`<my-lit-test></my-lit-test>`}
     </div>
   `;
 
-  const out = await consume(iter);
+  let out = await consume(iter);
   assertStringIncludes(out, '<div id="testing">Hello world</div>');
+});
+
+Deno.test('Can render lit-element attributes', async () => {
+  class MyElement extends LitElement {
+    render() {
+      return litHtml`<div>testing</div>`;
+    }
+  }
+  customElements.define('lit-element-attrs', MyElement);
+  let iter = html`
+    <div>${render`<lit-element-attrs foo="bar" two="three"></lit-element-attrs>`}</div>
+  `;
+  let out = await consume(iter);
+  let { document } = parseHTML(out);
+  let el = document.querySelector('lit-element-attrs');
+  assertEquals(el.getAttribute('foo'), 'bar');
+  assertEquals(el.getAttribute('two'), 'three');
 });
