8828bf6a5aab369bf1e322544b9be457589d0bd0
Matthew Phillips <matthew@phillipsoft.com>
2021-08-07 11:42:57 -0400

    Update the Ocean version

---
+++ lib/mod.d.ts
@@ -0,0 +1,18 @@
+export {
+  HTMLElement,
+  customElements,
+  document
+} from './dom.js';
+
+export {
+  addEventListener,
+  html,
+  route,
+  startServer
+} from './global.js';
+
+export { Beach } from './beach.js';
+
+export {
+  raw
+} from './render.js';
