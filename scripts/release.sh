41bdea5e3882298c59f684ea8a66acdef212d246
Matthew Phillips <matthew@phillipsoft.com>
2021-08-28 21:43:36 -0400

    Adds a release script

---
+++ scripts/release.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+while getopts "hv:" opt; do
+	case ${opt} in
+		h )
+			usage;
+			;;
+		v )
+			version="${OPTARG}"
+			;;
+		t )
+			test=1
+			;;
+		\? )
+			echo "Invalid option -${OPTARG}"
+			exit 1
+			;;
+	esac
+done
+shift $((OPTIND -1))
+
+if [ -z "$version" ]
+then
+  echo "-v is required"
+  exit 1
+fi
+
+deno run --allow-read --allow-write --allow-run \
+  https://cdn.spooky.click/spooky-release/0.0.6/cmd.js \
+  --pkg name
+  --version "$version"
+  --files **/*.md
+  --files site/pages/*.js
