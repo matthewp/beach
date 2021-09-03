9e4db91d2fc22e714c8bd5f6427df135a7aade9a
Matthew Phillips <matthew@matthewphillips.info>
2021-09-02 17:51:01 -0700

    Remove dom from the main module export (#27)
    Co-authored-by: Matthew Phillips <matthew@phillipsoft.com>

--- lib/mod.d.ts
+++ lib/mod.d.ts
@@ -1,11 +1,13 @@
-export {
-  HTMLElement,
-  customElements,
-  document
-} from './dom.js';
-
 export { Beach } from './beach.js';
 
 export {
   raw
 } from './render.js';
+
+export {
+  markdownPage
+} from './markdown.js';
+
+export {
+  RouteMatch
+} from './route_match.js';
